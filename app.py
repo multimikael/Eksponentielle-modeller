edbefd65eb0369c0c83c53f9b53beb2cf25cd87b
Mikael <prizelive@live.dk>
2017-11-11 11:49:19 +0100

    Moved to class and added menubar

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
 import wx
 
+class mainFrame(wx.Frame):
+
+    def __init__(self, *args, **kwargs):
+        super(mainFrame, self).__init__(*args, **kwargs)
+
+        self.OnCreate()
+
+    def OnCreate(self):
+        menubar = wx.MenuBar()
+        fileMenu = wx.Menu()
+        quitItem = fileMenu.Append(wx.ID_EXIT, 'Quit', 'Quit Application')
+        menubar.Append(fileMenu, '&File')
+        self.SetMenuBar(menubar)
+
+        self.Bind(wx.EVT_MENU, self.OnQuit, quitItem)
+
+        self.SetTitle('Eksponentielle Modeller')
+        self.Centre()
+        self.Show()
+
+    def OnQuit(self, event):
+        self.Close()
+
 app = wx.App()
-frame = wx.Frame(None, wx.ID_ANY, "Eksponentielle Modeller")
-frame.Show()
+mainFrame(None)
 app.MainLoop()
