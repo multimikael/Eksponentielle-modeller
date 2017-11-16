305af367737896506bd3221d15fcd4f1b804525d
Mikael <prizelive@live.dk>
2017-11-16 15:41:12 +0100

    Removed widgets in MainFrame

--- app.py
+++ app.py
@@ -20,7 +20,6 @@ class MainFrame(wx.Frame):
     def __init__(self, parent, _store, **kwargs):
         super(MainFrame, self).__init__(parent, **kwargs)
         self.store = _store
-        self.store.subscribe(self.OnStoreChange)
         self.OnCreate()
 
     def OnCreate(self):
@@ -33,40 +32,13 @@ class MainFrame(wx.Frame):
         self.Bind(wx.EVT_MENU, self.OnQuit, quitItem)
 
         mainPanel = wx.Panel(self)
-        vbox = wx.BoxSizer(wx.VERTICAL)
-
-        self._plotPanel = wxmplot.plotpanel.PlotPanel(mainPanel, size=(1, 1))
-
-        btnPanel = wx.Panel(mainPanel)
-        hbox = wx.BoxSizer(wx.HORIZONTAL)
-        popupBtn = wx.Button(btnPanel, label='Popup')
-        drawBtn = wx.Button(btnPanel, label='Draw')
-        hbox.Add(popupBtn, 1, wx.EXPAND)
-        hbox.Add(drawBtn, 1, wx.EXPAND)
-        btnPanel.SetSizer(hbox)
-
-        popupBtn.Bind(wx.EVT_BUTTON, self.OnPopupButton)
-        drawBtn.Bind(wx.EVT_BUTTON, self.OnDrawButton)
-
-        vbox.Add(self._plotPanel, 3, wx.EXPAND)
-        vbox.Add(btnPanel, 1, wx.EXPAND)
-        mainPanel.SetSizer(vbox)
+        
         self.SetTitle('Eksponentielle Modeller')
         self.Centre()
         self.Show()
 
     def OnQuit(self, event):
         self.Close()
-
-    def OnPopupButton(self, event):
-        GridFrame(self)
-
-    def OnDrawButton(self, event):
-        self.store.dispatch({'type': ''})
-
-    def OnStoreChange(self):
-        x = np.linspace(0.0, 10.0, 100.0)
-        self._plotPanel.plot(x, np.exp(x**2.0), title='Expo test', xmax=10.0, ymax=10.0)
 
 store = pydux.create_store(lambda state, action: True if state is None else not state)
 store.subscribe(lambda: print(store.get_state()))
